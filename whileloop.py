953e5dfed989d5015033be3ec4308fda3b74f544
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-19 13:01:59 +0800

    while loop

--- whileloop.py
+++ whileloop.py
@@ -1,3 +1,9 @@
 List = ['I', 'want', 'to', 'print', 'each', 'element']
 
+#method 1
 print("{}\n{}\n{}\n{}\n{}\n{}".format(List[0], List[1], List[2], List[3], List[4], List[5]))
+
+i=0
+while i!=len(list):
+    print(list[i])
+    i+=1
