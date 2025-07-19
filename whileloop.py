ef1af3d964a590c872d8157d4b1926296fdd406c
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-07-19 13:14:54 +0800

    while loop

--- whileloop.py
+++ whileloop.py
@@ -4,10 +4,18 @@ List = ['I', 'want', 'to', 'print', 'each', 'element']
 print("{}\n{}\n{}\n{}\n{}\n{}".format(List[0], List[1], List[2], List[3], List[4], List[5]))
 
 i=0
-while i!=len(list):
-    print(list[i])
+while i!=len(List):
+    print(List[i])
     i+=1
 i=1
 while i<=10:
     print(i)
     i+=1
+g=2
+while g<=20:
+    print(g)
+    g+=2
+f=1
+while f<=53:
+    print(f)
+    f+=2
